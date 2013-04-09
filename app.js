6014677e30d0788d3782c128cb63b51cd98b14f2
elishowk <elishowk@nonutc.fr>
2013-04-09 14:49:52 +0200

    simplified demo code

--- app.js
+++ app.js
@@ -6,7 +6,6 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
-var WidgetModel = require('./lib/db').model('widget');
 var parseMFQuery = require('./lib/fragmentparser.js');
 
 /**
@@ -55,43 +54,35 @@ app.configure(function () {
 /**
  * middleware: router
  */
-app.param('id', /^[a-f0-9]{24}$/);
 
-app.get('/:id', function (req, res) {
-  WidgetModel.findById(req.params.id, function (err, data) {
-    if (err) {
-      return res.status(500).json(err);
-    }
-    var validFragment = {};
-    var value;
-    for (var key in req.query) {
-      if(req.query.hasOwnProperty(key)) {
-        if(parseMFQuery[key]) {
-          value = parseMFQuery[key](req.query[key].toString());
-        }
-        if (!value) {
-          continue;
-        }
-        // keys may appear more than once,
-        // thus store all values in an array,
-        // the exception being "t"
-        if (!validFragment[key]) {
-          validFragment[key] = [];
-        }
-        if (key !== 't') {
-          validFragment[key].push(value);
-        } else {
-          validFragment[key][0] = value;
-        }
+app.get('/', function (req, res) {
+  var validFragment = {};
+  var value;
+  for (var key in req.query) {
+    if(req.query.hasOwnProperty(key)) {
+      if(parseMFQuery[key]) {
+        value = parseMFQuery[key](req.query[key].toString());
+      }
+      if (!value) {
+        continue;
+      }
+      // keys may appear more than once,
+      // thus store all values in an array,
+      // the exception being "t"
+      if (!validFragment[key]) {
+        validFragment[key] = [];
+      }
+      if (key !== 't') {
+        validFragment[key].push(value);
+      } else {
+        validFragment[key][0] = value;
       }
     }
-    res.render('proxy.jade', {
-      'parentUrl': data.permalink,
-      'fragment': JSON.stringify(validFragment)
-    });
+  }
+  res.render('proxy.jade', {
+    'fragment': JSON.stringify(validFragment)
   });
 });
-
 
 /**
  * listen (on socket)
